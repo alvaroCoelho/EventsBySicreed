35dc37583c388fb17e0a6c39940b0fcf126f734e
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 15:26:33 -0300

    Create Module.kt

---
+++ app/src/main/java/com/br/eventbysicreed/di/Module.kt
@@ -0,0 +1,39 @@
+package com.br.eventbysicreed.di
+
+import com.br.eventbysicreed.data.remote.ServiceApi
+import com.br.eventbysicreed.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Module {
+
+    @Provides
+    @Singleton
+    fun provideOkKttpClient(): OkHttpClient{
+        return OkHttpClient()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideServiceApi(retrofit: Retrofit): ServiceApi{
+        return retrofit.create(ServiceApi::class.java)
+    }
+}
