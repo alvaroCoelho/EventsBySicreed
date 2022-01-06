18057b4d60e84f617f1c640bc3ccb6f3950625ed
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 15:50:55 -0300

    Create show,hide and limitDescription

--- app/src/main/java/com/br/eventbysicreed/util/Extension.kt
+++ app/src/main/java/com/br/eventbysicreed/util/Extension.kt
@@ -1,5 +1,6 @@
 package com.br.eventbysicreed.util
 
+import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 
@@ -7,4 +8,20 @@ fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_LONG){
     Toast.makeText(requireContext(),
     message,
     duration).show()
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
+fun View.hide() {
+    visibility = View.INVISIBLE
+}
+
+fun String.limitDescription(characters: Int): String {
+    if (this.length > characters) {
+        val firstCharacter = 0
+        return "${this.substring(firstCharacter, characters)}..."
+    }
+    return this
 }
