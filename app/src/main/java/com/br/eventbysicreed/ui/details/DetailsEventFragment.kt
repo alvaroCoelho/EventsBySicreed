0147179217c105265694c4cbfa0da689934de624
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 10:17:43 -0300

    Create DetailsEventFragment.kt

---
+++ app/src/main/java/com/br/eventbysicreed/ui/details/DetailsEventFragment.kt
@@ -0,0 +1,17 @@
+package com.br.eventbysicreed.ui.details
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.br.eventbysicreed.databinding.FragmentDetailsEventBinding
+import com.br.eventbysicreed.ui.base.BaseFragment
+
+class DetailsEventFragment : BaseFragment<FragmentDetailsEventBinding, DetailsEventViewModel>() {
+    override val viewModel: DetailsEventViewModel by viewModels()
+
+    override fun getViewBiding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentDetailsEventBinding = FragmentDetailsEventBinding.inflate(
+        inflater, container,false)
+}
