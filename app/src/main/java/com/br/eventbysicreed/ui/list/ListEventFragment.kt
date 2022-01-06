8f14557df26462a56cc89b9c0e72ec1b982c18e9
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 09:53:21 -0300

    Create ListEventFragment.kt

---
+++ app/src/main/java/com/br/eventbysicreed/ui/list/ListEventFragment.kt
@@ -0,0 +1,19 @@
+package com.br.eventbysicreed.ui.list
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.br.eventbysicreed.databinding.FragmentListEventBinding
+import com.br.eventbysicreed.ui.base.BaseFragment
+
+class ListEventFragment : BaseFragment<FragmentListEventBinding, ListEventViewModel>() {
+
+    override val viewModel: ListEventViewModel by viewModels()
+
+
+    override fun getViewBiding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentListEventBinding = FragmentListEventBinding.inflate(
+        inflater, container,false)
+}
