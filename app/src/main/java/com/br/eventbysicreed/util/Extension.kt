8b7dc4ee536016db3b6efef73fec9768c0cb1fe7
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 15:48:40 -0300

    Create Extension.kt

---
+++ app/src/main/java/com/br/eventbysicreed/util/Extension.kt
@@ -0,0 +1,10 @@
+package com.br.eventbysicreed.util
+
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+fun Fragment.toast(message: String, duration: Int = Toast.LENGTH_LONG){
+    Toast.makeText(requireContext(),
+    message,
+    duration).show()
+}
