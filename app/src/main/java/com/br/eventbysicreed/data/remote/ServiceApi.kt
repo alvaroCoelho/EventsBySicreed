de8b963146410f8d9c7a9fa9dd430631254ce2cf
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 15:14:11 -0300

    Create ServiceApi.kt

---
+++ app/src/main/java/com/br/eventbysicreed/data/remote/ServiceApi.kt
@@ -0,0 +1,11 @@
+package com.br.eventbysicreed.data.remote
+
+import com.br.eventbysicreed.data.model.event.EventModel
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface ServiceApi {
+
+    @GET("events")
+    suspend fun list(): Response<EventModel>
+}
