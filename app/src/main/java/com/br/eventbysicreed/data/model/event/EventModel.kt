cbd0a7ca40b4c10f58119248205631f34dd2fe0e
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 09:39:26 -0300

    Create EventModel.kt and PeopleModel.kt

--- app/src/main/java/com/br/eventbysicreed/data/model/event/EventModel.kt
+++ app/src/main/java/com/br/eventbysicreed/data/model/event/EventModel.kt
@@ -1,16 +1,28 @@
 package com.br.eventbysicreed.data.model.event
 
+import com.br.eventbysicreed.data.model.people.PeopleModel
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class EventModel(
 
+    @SerializedName("id")
     val id:Int,
+    @SerializedName("title")
     val title:String,
+    @SerializedName("price")
     val price:String,
+    @SerializedName("latitude")
     val latitude:String,
+    @SerializedName("longitude")
     val longitude:String,
+    @SerializedName("image")
     val image:String,
+    @SerializedName("description")
     val description:String,
-    val date:String
+    @SerializedName("date")
+    val date:String,
+    @SerializedName("people")
+    val people: List<PeopleModel>
 
 ):Serializable
---
+++ app/src/main/java/com/br/eventbysicreed/data/model/people/PeopleModel.kt
@@ -0,0 +1,8 @@
+package com.br.eventbysicreed.data.model.people
+
+import java.io.Serializable
+
+data class PeopleModel(
+
+    val id:Int
+):Serializable
