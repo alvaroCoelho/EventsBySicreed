ec5f7926a089026491151daca9fe51f8f9fae960
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-06 17:45:58 -0300

    Create fetch, safeFetch , handleResponse on ListEventViewModel.kt

--- app/src/main/java/com/br/eventbysicreed/ui/list/ListEventViewModel.kt
+++ app/src/main/java/com/br/eventbysicreed/ui/list/ListEventViewModel.kt
@@ -1,6 +1,55 @@
 package com.br.eventbysicreed.ui.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.br.eventbysicreed.data.model.event.EventModel
+import com.br.eventbysicreed.repository.Repository
+import com.br.eventbysicreed.ui.state.ResourceState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class ListEventViewModel: ViewModel() {
+@HiltViewModel
+class ListEventViewModel @Inject constructor(
+    private val repository: Repository
+) : ViewModel() {
+
+    private val _list = MutableStateFlow<ResourceState<EventModel>>(ResourceState.Loading())
+    val list: StateFlow<ResourceState<EventModel>> = _list
+
+    init {
+        fetch()
+    }
+
+    private fun fetch() = viewModelScope.launch{
+        safeFetch()
+
+    }
+
+    private suspend fun safeFetch() {
+        try {
+            val response = repository.list()
+            _list.value = handleResponse(response)
+        }catch (throwable: Throwable){
+            when(throwable){
+                is IOException -> _list.value = ResourceState.Error("Erro conexão")
+                else -> _list.value = ResourceState.Error("Falha conversão")
+            }
+        }
+
+    }
+
+    private fun handleResponse(response: Response<EventModel>): ResourceState<EventModel> {
+        if (response.isSuccessful){
+            response.body()?.let { values ->
+                return ResourceState.Sucess(values)
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
+
 }
