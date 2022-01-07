25ff505e2c6701b369b5c79c5a6d181d78d43f8e
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-07 10:47:07 -0300

    insert getEvent

--- app/src/main/java/com/br/eventbysicreed/data/remote/ServiceApi.kt
+++ app/src/main/java/com/br/eventbysicreed/data/remote/ServiceApi.kt
@@ -3,9 +3,20 @@ package com.br.eventbysicreed.data.remote
 import com.br.eventbysicreed.data.model.event.EventModel
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ServiceApi {
 
     @GET("events")
     suspend fun list(): Response<List<EventModel>>
+
+
+    @GET("events/{id}")
+    suspend fun getEvent(
+        @Path(
+            value = "idd",
+            encoded = true
+        )characterId: Int
+
+    ): Response<EventModel>
 }
