b1860d9394a300a41a2ab7798ac13e311b8a46ad
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-07 11:47:36 -0300

    Create onViewCreated,onLoadEvent

--- app/src/main/java/com/br/eventbysicreed/ui/details/DetailsEventFragment.kt
+++ app/src/main/java/com/br/eventbysicreed/ui/details/DetailsEventFragment.kt
@@ -1,8 +1,12 @@
 package com.br.eventbysicreed.ui.details
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.br.eventbysicreed.data.model.event.EventModel
 import com.br.eventbysicreed.databinding.FragmentDetailsEventBinding
 import com.br.eventbysicreed.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,6 +14,20 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class DetailsEventFragment : BaseFragment<FragmentDetailsEventBinding, DetailsEventViewModel>() {
     override val viewModel: DetailsEventViewModel by viewModels()
+
+    private val args: DetailsEventFragmentArgs by navArgs()
+     private lateinit var eventModel: EventModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        eventModel = args.event
+        onLoadEvent(eventModel)
+    }
+
+    private fun onLoadEvent(eventModel: EventModel)= with(binding){
+        tvNameEventDetails.text = eventModel.title
+
+    }
 
     override fun getViewBiding(
         inflater: LayoutInflater,
