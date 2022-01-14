483c67002a5872b500bf535cd5f40cdfc8a78cd1
alvaroCoelho <alvarofreitascoelho@gmail.com>
2022-01-14 09:58:58 -0300

    BASE_URL Constants.kt

--- app/src/main/java/com/br/eventbysicreed/util/Constants.kt
+++ app/src/main/java/com/br/eventbysicreed/util/Constants.kt
@@ -1,5 +1,5 @@
 package com.br.eventbysicreed.util
 
 object Constants {
-    const val BASE_URL = "http://5f5a8f24d44d640016169133.mockapi.io/api/"
+    const val BASE_URL = "https://api.spacexdata.com/v3/"
 }
